f76a8031bc64b3d31473859831ed807e7ea6b0e9
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-12-09 00:53:06 -0500

    complete lesson 2, use typescript and create an interface

--- index.ts
+++ index.ts
@@ -2,7 +2,21 @@ import axios from 'axios';
 
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number,
+  title: string,
+  completed: boolean;
+}
 
-axios.get(url).then(response => {
-  console.log(response.data)
-})
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+The TODO with an ID: ${ID}
+Has a title of: ${title}
+Is it finished: ${finished}
+`);
+});
